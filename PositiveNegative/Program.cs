fa0075042d85b2468a76ee59639197812978a956
Opilane TTHK <opilane@tthk.ee>
2021-09-28 14:16:07 +0300

    commit

---
+++ PositiveNegative/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PositiveNegative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*The program checks if the number is positive or negative*/
+            Console.WriteLine("Enter your number: ");
+
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            if(userNumber > 0)
+            {
+                Console.WriteLine("The number is positive.");
+            } 
+            else
+            {
+                Console.WriteLine("The number is negative.");
+            }
+
+        }
+    }
+}
